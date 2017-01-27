03854509b4a240b51883c140e08f43f545a9e2eb
Nate Hazelton <natehazelnut@gmail.com>
2017-01-27 09:23:46 -0500

    started person5 method, just seeing if this works

--- src/main/java/edu/gatech/oad/antlab/person/Person5.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person5.java
@@ -30,8 +30,10 @@ public class Person5 {
 	 * @return the modified string
 	 */
 	private String calc(String input) {
-	  //Person 5 put your implementation here
-	  return null;
+	  
+	  String str = input;
+	  
+
 	}
 	
 	/**
